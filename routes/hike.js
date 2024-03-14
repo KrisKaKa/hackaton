c68492d5ae2f6b054b4532d1f7637dd3157d0832
Kris KaKa <k.kula.main@gmail.com>
2024-03-14 08:02:25 +0100

    Syntax3

--- routes/hike.js
+++ routes/hike.js
@@ -81,7 +81,7 @@ exports.myEvents = function(req, res) {
   'E.EVENT_DATE, ' +
   'E.NAME,' +
   'E.TYPE,' + 
-  'if ( E.EVENT_ID in (Select * from CUST_EVENTS where CUSTOMER_STRAVA=' +input.CUSTOMER_STRAVA +  "), 'true','false' as participate " + 
+  'if ( E.EVENT_ID in (Select * from CUST_EVENTS where CUSTOMER_STRAVA=' +input.CUSTOMER_STRAVA +  "), 'true','false') as participate " + 
   'FROM EVENTS as E', function(err,
 rows) {
     if (err) {
