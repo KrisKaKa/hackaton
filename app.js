4a5a251ac356ea4c0997bde8a3b4f4b4f67f0ba6
agrabara <agrabara@gmail.com>
2024-03-14 07:42:17 +0100

    dodany czas

--- app.js
+++ app.js
@@ -123,6 +123,9 @@ async.series([
                       'TYPE VARCHAR(40), ' +
                       'NUMBER_LEFT INT,' +
                       'SI VARCHAR(40), ' +
+                      'DAYS_LEFT INT,' +
+                      'HOURS_LEFT INT,' +
+                      'MINUTES_LEFT INT,' +
                       'PRIMARY KEY(DETAILS_ID))', callback);
 },
   function insert_default(callback) {
@@ -157,28 +160,33 @@ async.series([
   }
   ,
   function insert_default4(callback) {
-    var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Running', NUMBER_LEFT:5, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb4'};
+    var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Running', NUMBER_LEFT:5, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb4', DAYS_LEFT: 2, HOURS_LEFT: 4, MINUTES_LEFT:55};
     client.query('INSERT INTO DETAILS set ?', detail, callback);
     console.log('dodalem rekordy do bazy DETAILS')
   },
   function insert_default5(callback) {
-      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Running', NUMBER_LEFT:16, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb5'};
+      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Biking', NUMBER_LEFT:16, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb5', DAYS_LEFT: 0, HOURS_LEFT: 3, MINUTES_LEFT:45};
     client.query('INSERT INTO DETAILS set ?', detail, callback);
     console.log('dodalem rekordy do bazy DETAILS')
   }
   ,
   function insert_default8(callback) {
-    var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Running', NUMBER_LEFT:10, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb6'};
+    var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '32299812', TYPE: 'Running', NUMBER_LEFT:10, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb6', DAYS_LEFT: 22, HOURS_LEFT: 11, MINUTES_LEFT:5};
     client.query('INSERT INTO DETAILS set ?', detail, callback);
     console.log('dodalem rekordy do bazy DETAILS')
   },
   function insert_default9(callback) {
-      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '2598605', TYPE: 'Running', NUMBER_LEFT:6, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb4'};
+      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '2598605', TYPE: 'Running', NUMBER_LEFT:6, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb4', DAYS_LEFT: 2, HOURS_LEFT: 4, MINUTES_LEFT:55};
     client.query('INSERT INTO DETAILS set ?', detail, callback);
     console.log('dodalem rekordy do bazy DETAILS')
   },
   function insert_default10(callback) {
-      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '2598605', TYPE: 'Running', NUMBER_LEFT:4, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb7'};
+      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '2598605', TYPE: 'Running', NUMBER_LEFT:4, SI: 'km', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb7', DAYS_LEFT: 1, HOURS_LEFT: 17, MINUTES_LEFT:3};
+    client.query('INSERT INTO DETAILS set ?', detail, callback);
+    console.log('dodalem rekordy do bazy DETAILS')
+  },
+  function insert_default11(callback) {
+      var detail = {DETAILS_ID: uuid.v4(), CUSTOMER_STRAVA: '2598605', TYPE: 'Other', NUMBER_LEFT:10, SI: 'h', EVENT_ID: 'bbbccda7-d6a1-461e-aec2-315ca8a07eb8', DAYS_LEFT: 12, HOURS_LEFT: 3, MINUTES_LEFT:21};
     client.query('INSERT INTO DETAILS set ?', detail, callback);
     console.log('dodalem rekordy do bazy DETAILS')
   }
